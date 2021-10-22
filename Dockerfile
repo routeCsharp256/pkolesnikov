f00474c5ec37b22e1e1496fc65ee0f2663b8733c
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-10-22 12:37:14 +0300

    Создан каркас сервиса, настроена сборка и развёртывание. (#1)
    * Создан каркас сервиса, настроена сборка и развёртывание.
    
    * Файл docker-compose перемещён в папку deploy. В самом файле отступы сделаны через пробелы.
    
    * Решение перемещено на папку выше. Убран таб в конце 10-й строки в файле docker-compose.
    
    * Удалён повторный файл docker-compose. В docker-compose прописано создание образа, а не использование имеющегося.

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+
+WORKDIR /src
+
+COPY ["src/OzonEdu.MerchandiseApi/OzonEdu.MerchandiseApi.csproj", "src/OzonEdu.MerchandiseApi/"]
+
+RUN dotnet restore "src/OzonEdu.MerchandiseApi/OzonEdu.MerchandiseApi.csproj"
+
+COPY . .
+
+WORKDIR "/src/src/OzonEdu.MerchandiseApi"
+
+RUN dotnet build "OzonEdu.MerchandiseApi.csproj" -c Release -o /app/build
+
+FROM build AS publish
+
+RUN dotnet publish "OzonEdu.MerchandiseApi.csproj" -c Release -o /app/publish
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS runtime
+
+WORKDIR /app
+
+EXPOSE 80
+EXPOSE 443
+
+FROM runtime AS final
+
+WORKDIR /app
+
+COPY --from=publish /app/publish .
+
+ENTRYPOINT ["dotnet", "OzonEdu.MerchandiseApi.dll"]
---
+++ src/OzonEdu.MerchandiseApi/Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OzonEdu.MerchandiseApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
