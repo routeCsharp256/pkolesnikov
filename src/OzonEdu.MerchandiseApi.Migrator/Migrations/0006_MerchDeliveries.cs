15d2ebb1e60bc72ffae7a1da30df56cf156ce710
Pyotr <2chilavert3@mail.ru>
2021-11-18 22:22:35 +0300

    Созданы миграции для MerchDelivery.

---
+++ src/OzonEdu.MerchandiseApi.Migrator/Migrations/0006_MerchDeliveries.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace OzonEdu.MerchandiseApi.Migrator.Migrations
+{
+    [Migration(6)]
+    public class MerchDeliveries : Migration 
+    {
+        private const string TableName = "merch_deliveries";
+        
+        public override void Up()
+        {
+            Create
+                .Table(TableName)
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("merch_delivery_status_id").AsInt32().NotNullable()
+                .WithColumn("merch_pack_type_id").AsInt32().NotNullable()
+                .WithColumn("status_change_date").AsDate().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table(TableName);
+        }
+    }
+}
---
+++ src/OzonEdu.MerchandiseApi.Migrator/Temp/0005_MerchDeliverySkuMaps.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace OzonEdu.MerchandiseApi.Migrator.Temp
+{
+    [Migration(5)]
+    public class MerchDeliverySkuMaps : Migration
+    {
+        private const string TableName = "merch_delivery_sku_maps";
+        
+        public override void Up()
+        {
+            Create
+                .Table(TableName)
+                .WithColumn("merch_delivery_id").AsInt32().NotNullable()
+                .WithColumn("sku_id").AsInt64().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table(TableName);
+        }
+    }
+}
